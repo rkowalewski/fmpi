ecda29aad7cf3c2af9de5fb9c910a5bfe1c384f1
Roger Kowalewski <roger@kowas.net>
2019-06-05 17:03:39 +0200

    increase communication computation overlap

--- include/AlltoAll.h
+++ include/AlltoAll.h
@@ -11,6 +11,7 @@
 
 // Other AllToAll Algorithms
 #include <Bruck.h>
+#include <Debug.h>
 #include <Factor.h>
 
 namespace alltoall {
@@ -72,21 +73,43 @@ inline void hypercube(
 
   auto mpi_datatype = mpi::mpi_datatype<value_type>::type();
 
-  auto recv_buf = std::unique_ptr<value_type[]>(new value_type[blocksize]);
+  auto recv_buf =
+      std::unique_ptr<value_type[]>(new value_type[blocksize * 2]);
   auto merge_buf =
       std::unique_ptr<value_type[]>(new value_type[blocksize * nr]);
 
   auto* it_out  = &(*out);
   auto* it_mbuf = merge_buf.get();
 
+  auto reqs = std::array<MPI_Request, 2>{MPI_REQUEST_NULL};
+
+  auto partner = me ^ 1;
+  auto half    = 0;
+
+  MPI_Irecv(
+      recv_buf.get() + half * blocksize,
+      blocksize,
+      mpi_datatype,
+      partner,
+      100,
+      comm,
+      &reqs[0]);
+
+  MPI_Send(
+      &(*(begin + partner * blocksize)),
+      blocksize,
+      mpi_datatype,
+      partner,
+      100,
+      comm);
+
   std::copy(
       begin + me * blocksize, begin + me * blocksize + blocksize, it_out);
 
   auto recvcount = blocksize;
 
-  for (int i = 1; i < nr; ++i) {
-    auto partner = me ^ i;
-
+  for (int i = 2; i < nr; ++i) {
+#if 0
     auto res = MPI_Sendrecv(
         std::addressof(*(begin + partner * blocksize)),
         blocksize,
@@ -100,20 +123,57 @@ inline void hypercube(
         100,
         comm,
         MPI_STATUS_IGNORE);
+#endif
 
+    auto res = MPI_Wait(&reqs[0], MPI_STATUS_IGNORE);
     ASSERT(res == MPI_SUCCESS);
+
+    // next round
+    partner       = me ^ i;
+    auto prevHalf = half;
+    half          = 1 - prevHalf;
+
+    MPI_Irecv(
+        recv_buf.get() + half * blocksize,
+        blocksize,
+        mpi_datatype,
+        partner,
+        100,
+        comm,
+        &reqs[0]);
+
+    MPI_Send(
+        &(*(begin + partner * blocksize)),
+        blocksize,
+        mpi_datatype,
+        partner,
+        100,
+        comm);
 
     op(it_out,
        it_out + recvcount,
-       recv_buf.get(),
-       recv_buf.get() + blocksize,
+       recv_buf.get() + prevHalf * blocksize,
+       recv_buf.get() + (prevHalf + 1) * blocksize,
        it_mbuf);
 
-    std::swap(it_out, it_mbuf);
     recvcount += blocksize;
+
+    std::swap(it_out, it_mbuf);
   }
-  if (&(*it_out) != &(*out)) {
-    std::copy(it_out, it_out + recvcount, out);
+
+  auto res = MPI_Wait(&reqs[0], MPI_STATUS_IGNORE);
+  ASSERT(res == MPI_SUCCESS);
+
+  op(it_out,
+     it_out + recvcount,
+     recv_buf.get() + half * blocksize,
+     recv_buf.get() + (half + 1) * blocksize,
+     it_mbuf);
+
+  recvcount += blocksize;
+
+  if (&(*it_mbuf) != &(*out)) {
+    std::copy(it_mbuf, it_mbuf + recvcount, out);
   }
   ASSERT(recvcount == blocksize * nr);
   ASSERT(std::is_sorted(it_out, it_out + nr * blocksize));
@@ -123,7 +183,7 @@ template <class InputIt, class OutputIt, class Op>
 inline void MpiAlltoAll(
     InputIt begin, OutputIt out, int blocksize, MPI_Comm comm, Op&& /*op*/)
 {
-  using value_type = typename std::iterator_traits<InputIt>::value_type;
+  using value_type  = typename std::iterator_traits<InputIt>::value_type;
   auto mpi_datatype = mpi::mpi_datatype<value_type>::type();
 
   int nr;
@@ -140,25 +200,30 @@ inline void MpiAlltoAll(
 
   ASSERT(res == MPI_SUCCESS);
 
+#if 1
   std::vector<std::pair<OutputIt, OutputIt>> seqs;
   seqs.reserve(nr);
 
   for (size_t i = 0; i < std::size_t(nr); ++i) {
-    seqs.push_back(std::make_pair(out, out + blocksize));
+    seqs.push_back(
+        std::make_pair(out + i * blocksize, out + (i + 1) * blocksize));
   }
 
   auto merge_buf =
       std::unique_ptr<value_type[]>(new value_type[blocksize * nr]);
 
-  __gnu_parallel::multiway_merge(seqs.begin(),
-       seqs.end(),
-       merge_buf.get(),
-       blocksize, std::less<value_type>{},
-       __gnu_parallel::sequential_tag{});
+  __gnu_parallel::multiway_merge(
+      seqs.begin(),
+      seqs.end(),
+      merge_buf.get(),
+      blocksize * nr,
+      std::less<value_type>{},
+      __gnu_parallel::sequential_tag{});
 
-    std::copy(merge_buf.get(), merge_buf.get() + blocksize * nr, out);
-
-  //std::sort(out, out + blocksize * nr);
+  std::copy(merge_buf.get(), merge_buf.get() + blocksize * nr, out);
+#else
+  std::sort(out, out + blocksize * nr);
+#endif
 }
 }  // namespace alltoall
 #endif
