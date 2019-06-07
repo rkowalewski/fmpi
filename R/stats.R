4c2c8698da2bc63e3933a281444e4f6cc39884af
Roger Kowalewski <roger@kowas.net>
2019-06-07 23:30:46 +0200

    added R scripts

---
+++ R/stats.R
@@ -0,0 +1,38 @@
+#!/usr/bin/env Rscript
+
+suppressMessages(library(readr))
+suppressMessages(library(dplyr))
+library(ggplot2)
+library(RColorBrewer)
+library(tikzDevice)
+#library(extrafont)
+
+
+if(!exists("summarySE", mode="function")) {
+    # Let us source the summary.R script
+    initial.options <- commandArgs(trailingOnly = FALSE)
+    file.arg.name <- "--file="
+    script.name <- sub(file.arg.name, "", initial.options[grep(file.arg.name, initial.options)])
+    script.basename <- dirname(script.name)
+    other.name <- file.path(script.basename, "./include/summary.R")
+    source(other.name)
+}
+
+#args = commandArgs(trailingOnly=TRUE)
+#
+#if (length(args)< 2) {
+#    stop("Usage: ./stats.R <infile>", call.=FALSE)
+#}
+
+#csv.data <- read.csv(f, header=TRUE, strip.white=TRUE)
+f <- file("stdin")
+df.in <- read_csv(paste(collapse = "\n", readLines(f)), col_names=TRUE, col_types="iiiiicid")
+close(f)
+
+df.summary <- summarySE(df.in,
+                          measurevar="Time",
+                          groupvars=c("Nodes", "Procs", "Round", "Algo"),
+                          na.rm=TRUE)
+
+cat(format_csv(df.summary))
+
