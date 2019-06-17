e76f943dbc2a0134ac78166f3de871bd1a913322
Roger Kowalewski <roger@kowas.net>
2019-06-17 16:44:15 +0200

    revert to old non-blocking communication

--- include/AlltoAll.h
+++ include/AlltoAll.h
@@ -91,7 +91,7 @@ inline void flatHandshake(
 
 template <class InputIt, class OutputIt, class Op>
 inline void hypercube(
-    InputIt begin, OutputIt out, int blocksize, MPI_Comm comm, Op&& op)
+    InputIt begin, OutputIt out, int blocksize, MPI_Comm comm, Op&& /*op*/)
 {
   int me, nr;
   MPI_Comm_rank(comm, &me);
@@ -109,128 +109,40 @@ inline void hypercube(
 
   auto mpi_datatype = mpi::mpi_datatype<value_type>::type();
 
-  auto recv_buf =
-      std::unique_ptr<value_type[]>(new value_type[blocksize * 2]);
-  auto merge_buf =
-      std::unique_ptr<value_type[]>(new value_type[blocksize * nr]);
-
-  auto* it_out  = &(*out);
-  auto* it_mbuf = merge_buf.get();
-
-  auto reqs = std::array<MPI_Request, 2>{MPI_REQUEST_NULL};
-
-  auto partner = me ^ 1;
-  auto half    = 0;
 
   auto trace = TimeTrace{me, "Hypercube"};
-
   trace.tick(COMMUNICATION);
 
-  MPI_Irecv(
-      recv_buf.get() + half * blocksize,
-      blocksize,
-      mpi_datatype,
-      partner,
-      100,
-      comm,
-      &reqs[0]);
+  for (int i = 1; i < nr; ++i) {
+    auto partner = me ^ i;
 
-  MPI_Send(
-      &(*(begin + partner * blocksize)),
-      blocksize,
-      mpi_datatype,
-      partner,
-      100,
-      comm);
-
-  trace.tock(COMMUNICATION);
-
-  std::copy(
-      begin + me * blocksize, begin + me * blocksize + blocksize, it_out);
-
-  auto recvcount = blocksize;
-
-  for (int i = 2; i < nr; ++i) {
-#if 0
     auto res = MPI_Sendrecv(
         std::addressof(*(begin + partner * blocksize)),
         blocksize,
         mpi_datatype,
         partner,
         100,
-        recv_buf.get(),
+        std::addressof(*(out + partner * blocksize)),
         blocksize,
         mpi_datatype,
         partner,
         100,
         comm,
         MPI_STATUS_IGNORE);
-#endif
-
-    trace.tick(COMMUNICATION);
-    auto res = MPI_Wait(&reqs[0], MPI_STATUS_IGNORE);
-    trace.tock(COMMUNICATION);
 
     ASSERT(res == MPI_SUCCESS);
-
-    // next round
-    partner       = me ^ i;
-    auto prevHalf = half;
-    half          = 1 - prevHalf;
-
-    trace.tick(COMMUNICATION);
-    MPI_Irecv(
-        recv_buf.get() + half * blocksize,
-        blocksize,
-        mpi_datatype,
-        partner,
-        100,
-        comm,
-        &reqs[0]);
-
-    MPI_Send(
-        &(*(begin + partner * blocksize)),
-        blocksize,
-        mpi_datatype,
-        partner,
-        100,
-        comm);
-
-    trace.tock(COMMUNICATION);
-
-    trace.tick(MERGE);
-    op(it_out,
-       it_out + recvcount,
-       recv_buf.get() + prevHalf * blocksize,
-       recv_buf.get() + (prevHalf + 1) * blocksize,
-       it_mbuf);
-    trace.tock(MERGE);
-
-    recvcount += blocksize;
-
-    std::swap(it_out, it_mbuf);
   }
 
-  trace.tick(COMMUNICATION);
-  auto res = MPI_Wait(&reqs[0], MPI_STATUS_IGNORE);
+  std::copy(
+      begin + me * blocksize,
+      begin + me * blocksize + blocksize,
+      out + me * blocksize);
+
   trace.tock(COMMUNICATION);
-  ASSERT(res == MPI_SUCCESS);
-
   trace.tick(MERGE);
-  op(it_out,
-     it_out + recvcount,
-     recv_buf.get() + half * blocksize,
-     recv_buf.get() + (half + 1) * blocksize,
-     it_mbuf);
-
-  recvcount += blocksize;
-
-  if (&(*it_mbuf) != &(*out)) {
-    std::copy(it_mbuf, it_mbuf + recvcount, out);
-  }
   trace.tock(MERGE);
-  ASSERT(recvcount == blocksize * nr);
 }
+
 
 template <class InputIt, class OutputIt, class Op>
 inline void MpiAlltoAll(
