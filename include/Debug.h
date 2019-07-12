1f6b931e0ed6946ac324c33a934c75cdf4ef3d48
Roger Kowalewski <roger@kowas.net>
2019-07-12 14:28:32 +0200

    rework for better assert macros

--- include/Debug.h
+++ include/Debug.h
@@ -5,23 +5,50 @@
 #include <iterator>
 #include <sstream>
 
-#define A2A_UNUSED(x) \
-  do {                \
-    (void)(x);        \
-  } while (0)
-
-#ifdef NDEBUG
-
-#define A2A_ASSERT(x) A2A_UNUSED(x)
-
-#define A2A_ASSERT_RETURNS(x, ret) \
-  A2A_UNUSED(x);                   \
-  A2A_UNUSED(ret)
+// Stolen from Abseil
+//
+// A2A_PREDICT_TRUE, A2A_PREDICT_FALSE
+//
+// Enables the compiler to prioritize compilation using static analysis for
+// likely paths within a boolean branch.
+//
+// Example:
+//
+//   if (A2A_PREDICT_TRUE(expression)) {
+//     return result;                        // Faster if more likely
+//   } else {
+//     return 0;
+//   }
+//
+// Compilers can use the information that a certain branch is not likely to be
+// taken (for instance, a CHECK failure) to optimize for the common case in
+// the absence of better information (ie. compiling gcc with
+// `-fprofile-arcs`).
+//
+// Recommendation: Modern CPUs dynamically predict branch execution paths,
+// typically with accuracy greater than 97%. As a result, annotating every
+// branch in a codebase is likely counterproductive; however, annotating
+// specific branches that are both hot and consistently mispredicted is likely
+// to yield performance improvements.
+#if (defined(__GNUC__) && !defined(__clang__))
+#define A2A_PREDICT_FALSE(x) (__builtin_expect(x, 0))
+#define A2A_PREDICT_TRUE(x) (__builtin_expect(!!(x), 1))
 #else
+#define A2A_PREDICT_FALSE(x) (x)
+#define A2A_PREDICT_TRUE(x) (x)
+#endif
 
-#include <cassert>
-#define A2A_ASSERT(x) assert(x)
-#define A2A_ASSERT_RETURNS(x, ret) A2A_ASSERT((x) == (ret))
+#if defined(NDEBUG)
+#define A2A_ASSERT(expr) \
+  (false ? static_cast<void>(expr) : static_cast<void>(0))
+
+#define A2A_ASSERT_RETURNS(expr, ret) \
+  (true ? static_cast<void>(expr) : static_cast<void>(0))
+#else
+#define A2A_ASSERT(expr)                           \
+  (A2A_PREDICT_TRUE((expr)) ? static_cast<void>(0) \
+                            : [] { assert(false && #expr); }())  // NOLINT
+#define A2A_ASSERT_RETURNS(expr, ret) A2A_ASSERT((expr) == (ret))
 #endif
 
 template <class InputIt>
