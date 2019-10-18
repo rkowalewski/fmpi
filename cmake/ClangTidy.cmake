9caf0fde4ab2e1a28ba2b5d554901627a8c0199e
Roger Kowalewski <roger@kowas.net>
2019-10-18 22:39:58 +0200

    improve clang tidy target

--- cmake/ClangTidy.cmake
+++ cmake/ClangTidy.cmake
@@ -27,22 +27,36 @@ if(CMAKE_VERSION VERSION_GREATER 3.6 AND ENABLE_CLANG_TIDY)
         message(FATAL_ERROR "unable to locate run-clang-tidy-5.0.py")
     endif()
 
+    execute_process(
+        COMMAND bash "-c" "git -C ${CMAKE_SOURCE_DIR} ls-files | grep \".*\\(external\\|libs\\|src\\).*\\.cc$\""
+        OUTPUT_VARIABLE TRACKED_FILES)
+
+
+    string(REPLACE "\n" ";" TRACKED_FILES ${TRACKED_FILES})
+
+    message(INFO "tracked files: ${TRACKED_FILES}")
+
     list(APPEND RUN_CLANG_TIDY_BIN_ARGS
         -clang-tidy-binary ${CLANG_TIDY_BIN}
-        "\"-header-filter=${CMAKE_SOURCE_DIR}/\(include\|libs\)/**\""
-        -checks="*,-hicpp-no-array-decay,-fuchsia*,-cppcoreguidelines-pro-bounds-array-to-pointer-decay,-clang-analyzer-core.NonNull*,-clang-analyzer-core.NullDereference,-clang-analyzer-core.uninitialized.Branch"
-        ${CMAKE_SOURCE_DIR}/src/*
-        ${CMAKE_SOURCE_DIR}/benchmark/src/*
+        "\"-header-filter=.*${CMAKE_SOURCE_DIR}.*/(include|libs|src)/.*\""
+        "\"-checks=*,\
+        -hicpp-no-array-decay,\
+        -fuchsia*,-cppcoreguidelines-pro-bounds-array-to-pointer-decay,\
+        -clang-analyzer-core.NonNull*,\
+        -clang-analyzer-core.NullDereference,\
+        -clang-analyzer-core.uninitialized.Branch\""
         )
 
     if (CLANG_TIDY_FIX)
         list(APPEND RUN_CLANG_TIDY_BIN_ARGS
-            "-fix")
+            -fix
+            -format
+            -style file)
     endif()
 
     add_custom_target(
         tidy
-        COMMAND ${RUN_CLANG_TIDY_BIN} ${RUN_CLANG_TIDY_BIN_ARGS}
+        COMMAND ${RUN_CLANG_TIDY_BIN} ${RUN_CLANG_TIDY_BIN_ARGS} ${TRACKED_FILES}
         COMMENT "running clang tidy"
         )
 endif()
