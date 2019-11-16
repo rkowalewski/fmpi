603497d46f342967fa9079e77bf515fd2ec4b5e3
Roger Kowalewski <roger@kowas.net>
2019-11-16 14:51:33 +0100

    print openmp environment settings in preamble

--- benchmark/src/Params.cc
+++ benchmark/src/Params.cc
@@ -134,7 +134,8 @@ void printBenchmarkPreamble(
     auto        split = var.find('=');
     if (split != std::string::npos &&
         (var.find("OMPI_") != std::string::npos ||
-         var.find("I_MPI") != std::string::npos)) {
+         var.find("I_MPI") != std::string::npos ||
+         var.find("OMP_") != std::string::npos)) {
       auto key = var.substr(0, split);
       auto val = var.substr(split + 1);
       trim(key);
