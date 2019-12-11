b94e1fabdde927eb80d838ede0d123a05e595a9c
Roger Kowalewski <roger@kowas.net>
2019-12-11 19:51:37 +0100

    minor changes

--- apps/mpiPinning.cc
+++ apps/mpiPinning.cc
@@ -4,12 +4,7 @@
 
 #include <iostream>
 #include <sstream>
-
-#include <fmpi/NumericRange.hpp>
-#include <fmpi/mpi/Environment.hpp>
-
-#include <rtlx/Assert.hpp>
-#include <rtlx/ScopedLambda.hpp>
+#include <vector>
 
 int main(int argc, char** argv)
 {
@@ -17,30 +12,18 @@ int main(int argc, char** argv)
   int provided;
   MPI_Init_thread(&argc, &argv, MPI_THREAD_FUNNELED, &provided);
 
-  auto finalizer = rtlx::scope_exit([]() { MPI_Finalize(); });
+  // rank
+  int me;
+  MPI_Comm_rank(MPI_COMM_WORLD, &me);
 
-  mpi::Context worldCtx{MPI_COMM_WORLD};
-
-  auto const me = worldCtx.rank();
-  // auto const nr = worldCtx.size();
-
+  // processor name
   char _hostname[MPI_MAX_PROCESSOR_NAME];
   int  resultlen;
-
-  RTLX_ASSERT_RETURNS(
-      MPI_Get_processor_name(_hostname, &resultlen), MPI_SUCCESS);
-
+  MPI_Get_processor_name(_hostname, &resultlen);
   std::string hostname(_hostname);
 
   std::vector<int> cpu_names(omp_get_max_threads());
-
-#if 0
-  {
-    std::ostringstream os;
-    os << "last cpu: " << worldCtx.getLastCPU() << "\n";
-    std::cout << os.str();
-  }
-#endif
+  int              mpi_cpu = sched_getcpu();
 
 #pragma omp parallel default(none) shared(cpu_names)
   {
@@ -50,18 +33,20 @@ int main(int argc, char** argv)
   }
 
   if (me == 0) {
-    std::cout << "Rank, CPU, Threads, ThreadID, Core\n";
+    std::cout << "Rank, CPU, RankCore, Threads, ThreadID, Core\n";
   }
 
-  MPI_Barrier(worldCtx.mpiComm());
+  MPI_Barrier(MPI_COMM_WORLD);
 
   std::ostringstream os;
-  for (auto&& r : fmpi::range(cpu_names.size())) {
-    os << me << ", " << hostname << ", " << cpu_names.size() << ", " << r
-       << ", " << cpu_names[r] << "\n";
+  for (std::size_t i = 0; i < cpu_names.size(); ++i) {
+    os << me << ", " << hostname << ", " << mpi_cpu << ", "
+       << cpu_names.size() << ", " << i << ", " << cpu_names[i] << "\n";
   }
 
   std::cout << os.str();
+
+  MPI_Finalize();
 
   return 0;
 }
