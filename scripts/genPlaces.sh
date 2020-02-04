4210e3b06835661041b953d6b72d654f74e84fbb
Roger Kowalewski <roger@kowas.net>
2020-02-04 09:19:34 +0100

    minor change in omp places script

--- scripts/genPlaces.sh
+++ scripts/genPlaces.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
 
-num_procs="$1"
+# Number of procs
+nprocs="$1"
 
+# Number of cores
 np="$(getconf _NPROCESSORS_ONLN)"
-procs_avail="${2:-$((np/2))}"
+ncores="${2:-$((np/2))}"
 
-comp_threads="$(((procs_avail / num_procs) - 1))"
+comp_threads="$(((ncores / nprocs) - 1))"
 comm_threads="1"
 
 domain_size="$((comp_threads + comm_threads))"
 
 _places=""
-for d in $(seq 0 $((num_procs-1)))
+for d in $(seq 0 $((nprocs-1)))
 do
   first_place="$((d*domain_size + comm_threads))"
   for p in $(seq $first_place $((first_place + comp_threads-1)))
   do
-    _places="$_places,{$p,$((p+$procs_avail))}"
+    _places="$_places,{$p,$((p+$ncores))}"
   done
 done
 
