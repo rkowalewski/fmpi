4ad226422673d700606677d4dfdb182445309bb1
Roger Kowalewski <roger@kowas.net>
2020-02-06 14:26:37 +0100

    ignore modernize-use-using in clang tidy

--- cmake/ClangTidy.cmake
+++ cmake/ClangTidy.cmake
@@ -42,8 +42,8 @@ if(CMAKE_VERSION VERSION_GREATER 3.6 AND ENABLE_CLANG_TIDY MATCHES "ON")
     # we cannot break multi line strings here
     list(APPEND RUN_CLANG_TIDY_BIN_ARGS
         -clang-tidy-binary ${CLANG_TIDY_BIN}
-        "\"-header-filter=.*${CMAKE_SOURCE_DIR}.*/(include|libs|src|benchmark)/.*\""
-        "\"-checks=*,-hicpp-no-array-decay,-fuchsia*,-cppcoreguidelines-pro-bounds-array-to-pointer-decay,-clang-analyzer-core.NonNull*,-clang-analyzer-core.NullDereference,-clang-analyzer-core.uninitialized.Branch,-modernize-concat-nested-namespaces,-modernize-use-trailing-return-type\""
+        "\"-header-filter=.*${CMAKE_SOURCE_DIR}/(include|libs|src|benchmark)/.*\""
+        "\"-checks=*,-hicpp-no-array-decay,-fuchsia*,-cppcoreguidelines-pro-bounds-array-to-pointer-decay,-clang-analyzer-core.NonNull*,-clang-analyzer-core.NullDereference,-clang-analyzer-core.uninitialized.Branch,-modernize-concat-nested-namespaces,-modernize-use-trailing-return-type,-modernize-use-using\""
         )
 
     list(APPEND RUN_CLANG_TIDY_BIN_ARGS
