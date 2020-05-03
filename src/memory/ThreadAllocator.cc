f540ddf3299d00955183620107ea8365a4e6a97c
Roger Kowalewski <roger@kowas.net>
2020-05-03 17:39:51 +0200

    remove -Werror and -Wredundant-decls

--- src/memory/ThreadAllocator.cc
+++ src/memory/ThreadAllocator.cc
@@ -1,5 +1,9 @@
 #include <fmpi/memory/ThreadAllocator.hpp>
 
+//#ifdef INTEL_COMPILER
+//#pragma warning(disable: 2196)  // warning #2196: routine is both "inline" and "noinline"
+//#endif
+
 #include <snmalloc.h>
 
 void* fmpi::snmalloc_alloc(std::size_t n) {
