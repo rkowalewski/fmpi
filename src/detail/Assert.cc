d60af0deee833097916c8ffe203db8cc06a5fc03
Roger Kowalewski <roger@kowas.net>
2020-05-18 09:06:45 +0200

    gracefully abort MPI if debug assert fails

--- src/detail/Assert.cc
+++ src/detail/Assert.cc
@@ -3,25 +3,35 @@
 #include <cstdio>
 #include <cstdlib>
 
+#include <fmpi/mpi/Environment.hpp>
 
 void fmpi::detail::handle_failed_assert(
     const char* msg, const char* file, int line, const char* fnc) noexcept {
+
+  auto const& world = mpi::Context::world();
+  constexpr int errc = 1;
+
   std::fprintf(
       stderr,
-      "[%s] Assertion failure in function %s (%s:%d): %s.\n",
+      "[%s, Rank %d] Assertion failure in function %s (%s:%d): %s.\n",
       FMPI_LOG_PREFIX,
+      static_cast<int>(world.rank()),
       fnc,
       file,
       line,
       msg);
+
+  MPI_Abort(world.mpiComm(), errc);
 }
 
 void fmpi::detail::handle_warning(
     const char* msg, const char* file, int line, const char* fnc) noexcept {
+  auto const& world = mpi::Context::world();
   std::fprintf(
       stderr,
-      "[%s] Warning triggered in function %s (%s:%d): %s.\n",
+      "[%s, Rank %d] Warning triggered in function %s (%s:%d): %s.\n",
       FMPI_LOG_PREFIX,
+      static_cast<int>(world.rank()),
       fnc,
       file,
       line,
